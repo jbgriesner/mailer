f46cf03a142b43141933f4367f34b8fdf64da989
jbgriesner <jb.grsr@gmail.com>
2024-06-28 19:14:07 +0000

    init - healthcheck

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
-fn main() {
-    println!("Hello, world!");
+use mailer_lib::run;
+use std::net::TcpListener;
+
+#[tokio::main]
+async fn main() -> Result<(), std::io::Error> {
+    let listener = TcpListener::bind("127.0.0.1:8000").expect("Failed to bind random port");
+
+    run(listener)?.await
 }
